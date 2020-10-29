2dade835cd08e9eda49550df1445fa69bf2e73db
Hasanur Rashid <hasanurrashid95@gmail.com>
2020-10-30 03:34:47 +0600

    Initial Spring Boot Project
    Support following dependencies:
    Spring Web
    Spring Security
    MySQL Driver
    MySQL Server Driver

---
+++ src/main/java/com/webapp/share4better/Share4betterApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.share4better;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Share4betterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Share4betterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webapp/share4better/Share4betterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.share4better;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Share4betterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
