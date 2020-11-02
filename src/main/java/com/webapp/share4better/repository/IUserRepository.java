7ded9bb56bc2fb4fe4e447a6069fb641a98cecd0
kmistry1 <kmistry1@uncc.edu>
2020-11-01 20:19:49 -0800

    profile update

--- src/main/java/com/webapp/share4better/repository/IUserRepository.java
+++ src/main/java/com/webapp/share4better/repository/IUserRepository.java
@@ -6,10 +6,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface IUserRepository extends CrudRepository<Profile, String> {
-    @Query(value = "select e FROM Users e WHERE e.user_name =(:userName)", nativeQuery = true)
+    @Query(value = "select FROM Users WHERE user_name =(:userName)", nativeQuery = true)
     public Iterable<Profile> userAuthentication(@Param("userName") String userName);
 
-    @Query(value = "select e FROM UserProfile e WHERE e.user_name = :userName", nativeQuery = true)
+    @Query(value = "select FROM UserProfile WHERE user_name = :userName", nativeQuery = true)
     public Iterable<Profile> userProfile(@Param("userName") String userName);
 
 
