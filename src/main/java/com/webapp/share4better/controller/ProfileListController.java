e194e76baead4ced2f82fdfd3b1bc0bd4371212d
kmistry1 <kmistry1@uncc.edu>
2020-11-21 09:24:49 -0800

    fixing signup page and adding backend

--- src/main/java/com/webapp/share4better/controller/ProfileListController.java
+++ src/main/java/com/webapp/share4better/controller/ProfileListController.java
@@ -1,15 +1,17 @@
 package com.webapp.share4better.controller;
 
 import com.webapp.share4better.model.Profile;
+import com.webapp.share4better.sevice.AddUserService;
 import com.webapp.share4better.sevice.IProfileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ProfileListController {
@@ -17,6 +19,8 @@ public class ProfileListController {
 
     @Autowired
     private IProfileService service;
+    @Autowired
+    private AddUserService userService;
 
     @PostMapping("/validateUser")
     @ResponseBody
@@ -43,26 +47,16 @@ public class ProfileListController {
 
     @PostMapping("/signUp")
     @ResponseBody
-    public ResponseEntity signupUser(@RequestParam("userEmail") String userEmail, @RequestParam("password") String password) {
+    public ResponseEntity signupUser(@RequestParam("userName") String userName, @RequestParam("userEmail") String userEmail, @RequestParam("password") String password) {
         Profile userProfile = new Profile();
-        try {
-            Iterable<Profile> profiles = service.getUserProfile(userEmail);
-            for (Profile profile : profiles) {
-                if (profile.getUserPassword().equals(password)) {
-                    userProfile = profile;
-                    logger.warn("VALID_USER");
-                } else {
-                    userProfile.setUserEmail(userEmail);
-                    logger.warn("INVALID_USER");
-                }
+        userProfile.setUserName(userName);
+        userProfile.setUserEmail(userEmail);
+        userProfile.setUserPassword(password);
+        userProfile.setDonorStatus(true);
 
-            }
-            return new ResponseEntity<>(userProfile, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+        userService.insertWithQuery(userProfile);
+        return new ResponseEntity<>(userProfile, HttpStatus.OK);
     }
-
 
 
 }
