73cbe6eec675adbd4f2f50a3e481d3f0f5382845
kmistry1 <kmistry1@uncc.edu>
2020-11-22 10:00:59 -0800

    adding password invalid fallback to retry

--- src/main/java/com/webapp/share4better/controller/ProfileListController.java
+++ src/main/java/com/webapp/share4better/controller/ProfileListController.java
@@ -9,11 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ProfileListController {
@@ -25,17 +30,25 @@ public class ProfileListController {
     private AddUserService userService;
 
     @PostMapping("/validateUser")
-    public String getUserProfile(@RequestParam("userEmail") String userEmail, @RequestParam("password") String password, HttpServletRequest httpServletRequest) {
+    public String getUserProfile(@RequestParam("userEmail") String userEmail, @RequestParam("password") String password, HttpServletRequest httpServletRequest,Model model) {
         Profile userProfile = new Profile();
-        String redirect = null;
         
             Iterable<Profile> profiles = service.getUserProfile(userEmail);
             for (Profile profile : profiles) {
                 if (profile.getUserPassword().equals(password)) {
-                    return "redirect:/user.html";
+                    @SuppressWarnings("unchecked")
+                    List<String> messages = (List<String>) httpServletRequest.getSession().getAttribute("USER");
+                    if (messages == null) {
+                        messages = new ArrayList<>();
+                        httpServletRequest.getSession().setAttribute("USER", messages);
+                    }
+                    messages.add(userEmail);
+                    httpServletRequest.getSession().setAttribute("USER", messages);
+                    model.addAttribute("user", messages);
+
+                    return "redirect:/welcome.html";
                 } else {
                     return "redirect:/index.html#id03";
-
                 }
 
             }
@@ -43,18 +56,41 @@ public class ProfileListController {
 
     }
 
+    @GetMapping("/")
+    public String process(Model model, HttpSession session) {
+
+        @SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) session.getAttribute("USER");
+
+        if (messages == null) {
+            messages = new ArrayList<>();
+        }
+
+
+        return "welcome";
+    }
+
 
     @PostMapping("/signUp")
-    @ResponseBody
-    public ResponseEntity signupUser(@RequestParam("userName") String userName, @RequestParam("userEmail") String userEmail, @RequestParam("password") String password) {
-        Profile userProfile = new Profile();
-        userProfile.setUserName(userName);
-        userProfile.setUserEmail(userEmail);
-        userProfile.setUserPassword(password);
-        userProfile.setDonorStatus(true);
+    public String signupUser(@RequestParam("userName") String userName, @RequestParam("userEmail") String userEmail, @RequestParam("password") String password) {
 
-        userService.insertWithQuery(userProfile);
-        return new ResponseEntity<>(userProfile, HttpStatus.OK);
+
+        Iterable<Profile> profiles = service.getUserProfile(userEmail);
+        for (Profile profile : profiles) {
+            if (profile.getUserEmail().equals(userEmail)) {
+                return "redirect:/index.html#id04";
+            } else {
+                Profile userProfile = new Profile();
+                userProfile.setUserName(userName);
+                userProfile.setUserEmail(userEmail);
+                userProfile.setUserPassword(password);
+                userProfile.setDonorStatus(true);
+                userService.insertWithQuery(userProfile);
+            }
+
+        }
+
+        return "redirect:/index.html#id05";
     }
 
 
