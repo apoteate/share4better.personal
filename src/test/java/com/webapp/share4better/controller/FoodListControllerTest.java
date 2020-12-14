cce068829a9486e483c18567313798d2744269ef
kmistry1 <kmistry1@uncc.edu>
2020-12-13 21:50:49 -0500

    fix test 3

--- src/test/java/com/webapp/share4better/controller/FoodListControllerTest.java
+++ src/test/java/com/webapp/share4better/controller/FoodListControllerTest.java
@@ -96,32 +96,32 @@ public class FoodListControllerTest {
 
 
     }
-//
-//    @Test
-//    public void getMyPendingFoods() {
-//        HttpServletRequest mockRequest = mock(HttpServletRequest.class, RETURNS_DEEP_STUBS);
-//
-//        when(mockRequest.getSession().getAttribute("userID")).thenReturn(9999);
-//        List<ReceiverFoodList> foodList = foodListController.getMyPendingFoods(mockRequest).getBody();
-//
-//        assertEquals("First Food Item : " + foodList.get(0).getName(), 111111111, foodList.get(0).getId());
-//        assertEquals("First Food Item : " + foodList.get(0).getName(), 9999, foodList.get(0).getContributorID());
-//        assertEquals("First Food Item : " + foodList.get(0).getName(), 8888, foodList.get(0).getReceiverID());
-//        assertEquals("First Food Item : " + foodList.get(0).getName(), "bagels", foodList.get(0).getName());
-//        assertEquals("First Food Item : " + foodList.get(0).getName(), "bread", foodList.get(0).getType());
-//        assertEquals("First Food Item : " + foodList.get(0).getName(), "10", foodList.get(0).getQuantity());
-//        assertEquals("First Food Item : " + foodList.get(0).getName(), "fresh", foodList.get(0).getQuality());
-//
-//        assertEquals("Second Food Item : " + foodList.get(1).getName(), 111111112, foodList.get(1).getId());
-//        assertEquals("Second Food Item : " + foodList.get(1).getName(), 9991, foodList.get(1).getContributorID());
-//        assertEquals("Second Food Item : " + foodList.get(1).getName(), 8881, foodList.get(1).getReceiverID());
-//        assertEquals("Second Food Item : " + foodList.get(1).getName(), "squash", foodList.get(1).getName());
-//        assertEquals("Second Food Item : " + foodList.get(1).getName(), "vegetable", foodList.get(1).getType());
-//        assertEquals("Second Food Item : " + foodList.get(1).getName(), "5", foodList.get(1).getQuantity());
-//        assertEquals("Second Food Item : " + foodList.get(1).getName(), "good", foodList.get(1).getQuality());
-//
-//
-//    }
+
+    @Test
+    public void getMyPendingFoods() throws IOException {
+        HttpServletRequest mockRequest = mock(HttpServletRequest.class, RETURNS_DEEP_STUBS);
+        HttpServletResponse mockResponse = mock(HttpServletResponse.class, RETURNS_DEEP_STUBS);
+
+        when(mockRequest.getSession().getAttribute("userID")).thenReturn(9999);
+        Iterable<Food> foodIterable = foodService.getAllAvailableFood(9999);
+
+        for (Food food:foodIterable){
+            if (food.getContributorID().equals(9991)){
+                foodListController.requestFoodBooking(food.getId(),mockRequest,mockResponse);
+                List<ReceiverFoodList> foodList = foodListController.getMyPendingFoods(mockRequest).getBody();
+                assertEquals("Second Food Item : " + foodList.get(1).getName(), 111111112, foodList.get(1).getId());
+                assertEquals("Second Food Item : " + foodList.get(1).getName(), 9991, foodList.get(1).getContributorID());
+                assertEquals("Second Food Item : " + foodList.get(1).getName(), 8881, foodList.get(1).getReceiverID());
+                assertEquals("Second Food Item : " + foodList.get(1).getName(), "squash", foodList.get(1).getName());
+                assertEquals("Second Food Item : " + foodList.get(1).getName(), "vegetable", foodList.get(1).getType());
+                assertEquals("Second Food Item : " + foodList.get(1).getName(), "5", foodList.get(1).getQuantity());
+                assertEquals("Second Food Item : " + foodList.get(1).getName(), "good", foodList.get(1).getQuality());
+
+            }
+        }
+
+
+    }
 //
 //    @Test
 //    public void requestFoodBooking() throws IOException {
